f5edad0c5c1106640733e810df0a2dc3ed17a11a
JoaoACastro <joaoaccastro@gmail.com>
2024-11-21 10:08:43 -0300

    create code base

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -8,25 +8,59 @@ namespace Models
     public class Reserva
     {
         public List<Pessoa> Hospedes {get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
 
-        public void CadastrarHospeder()
+        public Reserva()
         {
-            //List<Pessoa> Hospedes
+            Hospedes = new List<Pessoa>(); //Inicia a lista
         }
 
-        public void CadastrarSuite()
+        public void CadastrarSuite(Suite suite)
         {
-            // suite suite
+            if (suite == null)
+            {
+                throw new ArgumentException("A suiíte não pode ser nula.");
+            }
+            Suite = suite;
         }
+        
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            if (Suite == null)
+            {
+                throw new InvalidOperationException("É necessário cadastrar uma suíte antes de adicionar hóspedes.");
+            }
+            if (hospedes.Count > Suite.Capacidade)
+            {
+                throw new InvalidOperationException("O número de hóspedes excede a capacidade da suíte");
+            }
+
+            Hospedes = hospedes;
+        }
+
 
         public int ObterQuantidadeHospedes()
         {
-            return 
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
         {
-            return
+            if (Suite == null)
+            {
+                throw new InvalidOperationException("É necessário cadastrar uma suíte para calcular o valor da diária.");
+            }
+
+            decimal valorTotal = Suite.ValorDiaria * DiasReservados;
+
+            if (DiasReservados >= 10)
+            {
+                //aplicar desconto de 10% para acima de 10 dias
+                valorTotal *= 0.90m;
+            }
+
+            return valorTotal;
         }
     }
 
--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Models;
+
+var suite = new Suite("Premium", 3, 150.00M);
+
+var hospedes = new List<Pessoa>
+{
+    new Pessoa("James", "Deane"),
+    new Pessoa("Diego", "Higa"),
+    new Pessoa("Adam", "LZ"),
+    //new Pessoa("Ryan", "Tuerck"),
+};
+
+var reserva = new Reserva();
+reserva.CadastrarSuite(suite);
+reserva.CadastrarHospedes(hospedes);
+
+Console.WriteLine($"Quantidade de hóspedes: {reserva.ObterQuantidadeHospedes()}");
+reserva.DiasReservados = 10;
+Console.WriteLine($"Valor total da diária: {reserva.CalcularValorDiaria():C}");
